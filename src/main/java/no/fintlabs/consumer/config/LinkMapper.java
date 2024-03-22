f66a8a8b87e4e50a5d207effe7b509732ba1ad9d
Hknots <hknots99@gmail.com>
2024-03-22 11:11:06 +0100

    change hardcoded package names to use class name

--- src/main/java/no/fintlabs/consumer/config/LinkMapper.java
+++ src/main/java/no/fintlabs/consumer/config/LinkMapper.java
@@ -1,22 +1,23 @@
 package no.fintlabs.consumer.config;
 
 import com.google.common.collect.ImmutableMap;
+import no.fint.model.administrasjon.personal.*;
+import no.fint.model.felles.Person;
 
 import java.util.Map;
 
 public class LinkMapper {
 
     public static Map<String, String> linkMapper() {
-        return ImmutableMap.<String,String>builder()
-            .put("no.fint.model.administrasjon.personal.Arbeidsforhold", "/administrasjon/personal/arbeidsforhold")
-            .put("no.fint.model.administrasjon.personal.Personalressurs", "/administrasjon/personal/personalressurs")
-            .put("no.fint.model.administrasjon.personal.Variabellonn", "/administrasjon/personal/variabellonn")
-            .put("no.fint.model.administrasjon.personal.Fravar", "/administrasjon/personal/fravar")
-            .put("no.fint.model.administrasjon.personal.Fasttillegg", "/administrasjon/personal/fasttillegg")
-            .put("no.fint.model.administrasjon.personal.Fastlonn", "/administrasjon/personal/fastlonn")
-            .put("no.fint.model.administrasjon.personal.Personalmappe", "/administrasjon/personal/personalmappe")
-            .put("no.fint.model.administrasjon.personal.Person", "/administrasjon/personal/person")
-            .build();
+        return ImmutableMap.<String, String>builder()
+                .put(Arbeidsforhold.class.getName(), "/administrasjon/personal/arbeidsforhold")
+                .put(Personalressurs.class.getName(), "/administrasjon/personal/personalressurs")
+                .put(Variabellonn.class.getName(), "/administrasjon/personal/variabellonn")
+                .put(Fravar.class.getName(), "/administrasjon/personal/fravar")
+                .put(Fasttillegg.class.getName(), "/administrasjon/personal/fasttillegg")
+                .put(Fastlonn.class.getName(), "/administrasjon/personal/fastlonn")
+                .put(Person.class.getName(), "/administrasjon/personal/person")
+                .build();
     }
 
 }
